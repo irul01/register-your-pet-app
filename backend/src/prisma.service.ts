359c8f53a3ad11ca343e6ee43b79a341e2de6fb1
Simoon Sung <ssung@r2ware.com>
2025-08-12 01:43:22 +0900

    4번째.

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma/client'; // 진짜 엄청나게 헤맸던 부분. 이 링크를 통해 해결함. https://github.com/prisma/prisma/discussions/19669
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient
